b8df232b248b6314b1d4552d6295268fae6aa5b5
Nipkan <118575259+Nipkan@users.noreply.github.com>
2023-05-08 13:10:26 -0400

    X held item changes

--- data/battle/held_consumables.asm
+++ data/battle/held_consumables.asm
@@ -2,8 +2,8 @@
 
 ConsumableEffects:
 	db HELD_BERRY
-	db HELD_2
-	db HELD_5
+	db HELD_2 ; FULL RESTORE
+	db HELD_5 ; MAX ELIXER???
 	db HELD_HEAL_POISON
 	db HELD_HEAL_FREEZE
 	db HELD_HEAL_BURN
@@ -18,8 +18,8 @@ ConsumableEffects:
 	db HELD_SP_DEFENSE_UP
 	db HELD_ACCURACY_UP
 	db HELD_EVASION_UP
-	db HELD_38
-	db HELD_71
-	db HELD_ESCAPE
+	db HELD_38 ; unknown purpose. currently used for energy wall
+	db HELD_71 ; poké doll. would allow free escape for one encounter. (kinda lame?)
+	db HELD_ESCAPE ; also the poké doll? isn't actually consumed, used for smokeball
 	db HELD_CRITICAL_UP
 	db -1
--- data/battle/held_stat_up.asm
+++ data/battle/held_stat_up.asm
@@ -6,4 +6,5 @@ HeldStatUpItems:
 	dbw HELD_SP_DEFENSE_UP, BattleCommand_SpecialDefenseUp
 	dbw HELD_ACCURACY_UP,   BattleCommand_AccuracyUp
 	dbw HELD_EVASION_UP,    BattleCommand_EvasionUp
+	dbw HELD_LIGHT_SCREEN,		BattleCommand_SpecialDefenseUp2
 	db -1 ; end
